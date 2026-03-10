acbfd5bbf9d05478231667ca90310a7e93cd8eb2
sinaida-space <ida.amst@gmail.com>
2026-03-11 00:16:39 +0100

    fix router basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/subject_001">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/privacy" element={<PrivacyPolicy />} />
